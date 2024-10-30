69b5382ff23e5c0472b3d19ce61a4022c73e1703
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-10-30 20:42:28 +0530

    login page is done

--- src/componets/Body.jsx
+++ src/componets/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
 
 function Body() {
   return (
     <div>
       <Navbar/>
+      <Outlet/>
     </div>
   )
 }
--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Login() {
+  const [email, setEmailId] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async ()=>{
+    try {
+        const res = axios.post("http://localhost:3000/login", {
+            email,
+            password
+        })
+        console.log(res)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
   return (
+    <div className='flex justify-center my-10' >
+      <div className="card bg-base-300 w-96 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Welcome To Login</h2>
     <div>
-      Login
+
+<label className="form-control w-full max-w-xs my-2">
+  <div className="label">
+    <span className="label-text">Email</span>
+  </div>
+  <input type="text" onChange={(e)=> setEmailId(e.target.value)} value={email} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+</label>
+<label className="form-control w-full max-w-xs my-2">
+  <div className="label">
+    <span className="label-text">password</span>
+  </div>
+  <input type="text" value={password}  onChange={(e)=> setPassword(e.target.value)}  placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+</label>
+    </div>
+    <div className="card-actions justify-center my-2">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
