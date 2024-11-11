b21129e56ecc573c77c116404c94415872f41179
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-11-11 22:33:50 +0530

    updated

--- src/componets/requests.jsx
+++ src/componets/requests.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addRequests } from '../utils/requestsSlice'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constant'
 
 const Requests = () => {
     
     const dispatch = useDispatch()
-    const requests = useSelector((state)=> state.connection)
+    const requests = useSelector((state)=> state.requests)
     console.log(requests, "=requests===")
     const fetchRequests = async ()=>{
         try {
-            const res = await axios.get(BASE_URL + "/user/requests", {withCredentials:true})
+            const res = await axios.get(BASE_URL + "/user/requests/recieved", {withCredentials:true})
            console.log(res)
            dispatch(addRequests(res?.data?.data))
         } catch (error) {
@@ -23,7 +25,16 @@ const Requests = () => {
 
   return (
     <div>
-      requests
+       <div className='text-center my-10'>
+      <h1>Connections</h1>
+      {requests?.map((connections)=>(
+        <h1>{connections.fromUserId.firstName}</h1>
+      ))}
+    </div>
+    <div className="card-actions justify-center my-4">
+        <button className="btn btn-secondary">Reject</button>
+        <button className="btn btn-primary">Accept</button>
+      </div>
     </div>
   )
 }
