76f8b7f316b02a9bdc29e41e04e76fb27c7877f5
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-11-19 21:52:09 +0530

    updated

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -107,7 +107,7 @@ function Navbar() {
         <li>
           <Link
             to="/"
-            className="block py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
+            className="block py-2 pl-4 pr-4 text-lg text-white rounded lg:bg-transparent lg:text-white-700 dark:text-white"
             aria-current="page"
           >
             Home
@@ -116,7 +116,7 @@ function Navbar() {
         <li>
           <Link
             to="/connections"
-            className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+            className="block py-2 pl-4 pr-4 text-lg text-gray-500 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-white  dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
           >
             Connections
           </Link>
@@ -124,7 +124,7 @@ function Navbar() {
         <li>
           <Link
             to="/requests"
-            className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+            className="block py-2 pl-4 pr-4 text-lg text-gray-500 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-white  dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
           >
             Requests
           </Link>
@@ -132,7 +132,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+            className="block py-2 pl-4 pr-4 text-lg text-gray-500 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-white  dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
           >
             Contact
           </a>
